a38007ecd7d838adc000ec5a07c91a6a8c0b933f
ahmed <ahmedlearn01gmail.com>
2022-06-26 14:55:11 +0300

    initial commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+require('dotenv').config();
+
+const port = process.env.PORT || 5000
+
+const app = express();
+
+app.listen(port, () =>{
+    console.log(`server is running at port ${port}`)
+})
