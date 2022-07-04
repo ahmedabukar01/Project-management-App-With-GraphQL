3faf089a56d17595dc6ab915361a53ab1dff0ede
ahmednurdev <ahmednurdev@gmail.com>
2022-07-04 11:57:48 +0300

    connect db

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 require('dotenv').config();
+const colors = require('colors');
 const {graphqlHTTP} = require('express-graphql');
-const schema = require('./schema/schema')
+const schema = require('./schema/schema');
+const connectDB = require('./config/db');
 
 const port = process.env.PORT || 5000
 
 const app = express();
+
+// connect db
+connectDB();
 
 app.use('/graphql', graphqlHTTP({
     schema,
