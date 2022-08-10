e1a86f07178248ff2f331c7e134c799f5875aaa6
ahmed <ahmedlearn01@gmail.com>
2022-08-10 09:56:46 +0300

    apollo client added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import Header from "./Components/Header";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client'
+
+const client = new ApolloClient({
+  url: 'http://localhost:8000/graphql',
+  cache: new InMemoryCache()
+})
 
 function App() {
   return (
     <>
+    <ApolloProvider client={client}>
       <Header />
       <div className="container">
         <h1>Hello React Developers</h1>
       </div>
+      </ApolloProvider>
     </>
   );
 }
