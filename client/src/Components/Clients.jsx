7768456bb24ffef345506044d9225b772dc30e01
ahmed <ahmedlearn01@gmail.com>
2022-08-11 11:38:14 +0300

    loading spinner

--- client/src/Components/Clients.jsx
+++ client/src/Components/Clients.jsx
@@ -1,21 +1,12 @@
-import {gql, useQuery} from '@apollo/client'
+import { useQuery} from '@apollo/client'
 import ClientRow from './ClientRow'
-
-const GET_CLIENTS = gql`
-query GetClients {
-    clients {
-        id
-        name
-        email
-        phone
-    }
-}
-`
+import { GET_CLIENTS } from '../queries/clientQueries'
+import Spinner from './Spinner'
 
 const Clients = () => {
     const { loading, error, data } = useQuery(GET_CLIENTS)
 
-    if(loading) return <p>loading...</p>
+    if(loading) return <Spinner />
     if(error) return <p>error...</p>;
 
   return <>
