0841619ce76bdf78adec097cd3a378caefdb1cb1
ahmed <ahmedlearn01@gmail.com>
2022-08-14 16:05:19 +0300

    project info page

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import { GET_SINGLE_PRO } from "../queries/projects"
+import { useQuery } from "@apollo/client"
+import { useParams, Link } from "react-router-dom";
+import Spinner from "../Components/Spinner";
+import ClientInfo from "../Components/ClientInfo";
+
 
 const Project = () => {
+  const {id} = useParams();
+  const {loading, error , data} = useQuery(GET_SINGLE_PRO,{
+    variables: {id}
+  });
+
+  if(loading) return <Spinner />
+  if(error) return <p>Something Went Wrong...</p>
   return (
-    <div>
-        <h1>project</h1>
-    </div>
+    <>
+        {!loading && !error && (
+          <div className="mx-auto w-75 card p-5">
+            <Link to="/" className="btn btn-light btn-sm w-25 d-inline ms-auto">Go Back</Link>
+
+            <h1>{data.project.name}</h1>
+            <p>{data.project.description}</p>
+            <h5 className="mt-3">Project Status</h5>
+            <p className="lead">{data.project.status}</p>
+
+            <ClientInfo client={data.project.client}/>
+          </div>
+        )}
+    </>
   )
 }
 
--- client/src/queries/projects.js
+++ client/src/queries/projects.js
@@ -12,4 +12,22 @@ query GetProjects {
 }
 `
 
-export {GET_PROJECTS}
+// get single Query
+const GET_SINGLE_PRO = gql`
+query GetSinglePro($id: ID!) {
+    project(id: $id){
+        id
+        name
+        description
+        status
+        client {
+            id
+            name
+            email
+            phone
+        }
+    }
+}
+`
+
+export {GET_PROJECTS, GET_SINGLE_PRO}
